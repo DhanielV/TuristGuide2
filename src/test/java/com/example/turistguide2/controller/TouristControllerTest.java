86645b21f2c7aec903eee583d50d8614a3912871
Daniel Vive <dafi0001@stud.kea.dk>
2024-09-25 10:36:18 +0200

    GitActions initial commit

--- src/test/java/com/example/turistguide2/controller/TouristControllerTest.java
+++ src/test/java/com/example/turistguide2/controller/TouristControllerTest.java
@@ -16,6 +16,8 @@ import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TouristController.class)
@@ -72,7 +74,10 @@ class TouristControllerTest {
     }
 
     @Test
-    void saveAttraction() {
+    void saveAttraction() throws Exception {
+        mockMvc.perform(post("/attractions/save").sessionAttr("touristAttraction", this.touristAttraction))                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/attractions"));
+
     }
 
     @Test
